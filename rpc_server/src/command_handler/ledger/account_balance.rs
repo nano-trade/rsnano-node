e6478e7a6989467707aae87244c9753f636401f9
Gustav Schauwecker <service@simpago.com>
2025-02-27 10:57:18 +0100

    Refactor account sets

--- rpc_server/src/command_handler/ledger/account_balance.rs
+++ rpc_server/src/command_handler/ledger/account_balance.rs
@@ -1,5 +1,5 @@
 use crate::command_handler::RpcCommandHandler;
-use rsnano_core::Amount;
+use rsnano_ledger::LedgerSet;
 use rsnano_rpc_messages::{
     unwrap_bool_or_true, AccountArg, AccountBalanceArgs, AccountBalanceResponse,
     AccountBlockCountResponse,
@@ -8,31 +8,12 @@ use rsnano_rpc_messages::{
 impl RpcCommandHandler {
     pub(crate) fn account_balance(&self, args: AccountBalanceArgs) -> AccountBalanceResponse {
         let only_confirmed = unwrap_bool_or_true(args.include_only_confirmed);
-
-        let tx = self.node.ledger.read_txn();
-        let balance = if only_confirmed {
-            self.node
-                .ledger
-                .confirmed()
-                .account_balance(&tx, &args.account)
-                .unwrap_or(Amount::zero())
+        if only_confirmed {
+            let set = self.node.ledger.confirmed2();
+            get_account_balance(set, &args)
         } else {
-            self.node
-                .ledger
-                .any()
-                .account_balance(&tx, &args.account)
-                .unwrap_or(Amount::zero())
-        };
-
-        let receivable = self
-            .node
-            .ledger
-            .account_receivable(&tx, &args.account, only_confirmed);
-
-        AccountBalanceResponse {
-            balance,
-            pending: receivable,
-            receivable,
+            let set = self.node.ledger.any2();
+            get_account_balance(set, &args)
         }
     }
 
@@ -43,5 +24,16 @@ impl RpcCommandHandler {
         let tx = self.node.ledger.read_txn();
         let account = self.load_account(&tx, &args.account)?;
         Ok(AccountBlockCountResponse::new(account.block_count))
+    }
+}
+
+fn get_account_balance(set: impl LedgerSet, args: &AccountBalanceArgs) -> AccountBalanceResponse {
+    let balance = set.account_balance(&args.account);
+    let receivable = set.account_receivable(&args.account);
+
+    AccountBalanceResponse {
+        balance,
+        pending: receivable,
+        receivable,
     }
 }
