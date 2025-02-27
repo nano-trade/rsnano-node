023d2904c8786f52d7f2bba8b005bac2f2dceb38
Gustav Schauwecker <service@simpago.com>
2025-02-27 11:02:07 +0100

    Refactor ledger sets

--- rpc_server/src/command_handler/ledger/accounts_balances.rs
+++ rpc_server/src/command_handler/ledger/accounts_balances.rs
@@ -1,5 +1,5 @@
 use crate::command_handler::RpcCommandHandler;
-use rsnano_core::Amount;
+use rsnano_ledger::LedgerSet;
 use rsnano_rpc_messages::{
     unwrap_bool_or_true, AccountBalanceResponse, AccountsBalancesArgs, AccountsBalancesResponse,
 };
@@ -7,40 +7,36 @@ use std::collections::HashMap;
 
 impl RpcCommandHandler {
     pub(crate) fn accounts_balances(&self, args: AccountsBalancesArgs) -> AccountsBalancesResponse {
-        let tx = self.node.ledger.read_txn();
-        let mut balances = HashMap::new();
         let only_confirmed = unwrap_bool_or_true(args.include_only_confirmed);
-
-        for account in args.accounts {
-            let balance = if only_confirmed {
-                self.node
-                    .ledger
-                    .confirmed()
-                    .account_balance(&tx, &account)
-                    .unwrap_or(Amount::zero())
-            } else {
-                self.node
-                    .ledger
-                    .any()
-                    .account_balance(&tx, &account)
-                    .unwrap_or(Amount::zero())
-            };
-
-            let pending = self
-                .node
-                .ledger
-                .account_receivable(&tx, &account, only_confirmed);
-
-            balances.insert(
-                account,
-                AccountBalanceResponse {
-                    balance,
-                    pending,
-                    receivable: pending,
-                },
-            );
+        if only_confirmed {
+            let set = self.node.ledger.confirmed2();
+            get_account_balances(set, &args)
+        } else {
+            let set = self.node.ledger.any2();
+            get_account_balances(set, &args)
         }
-
-        AccountsBalancesResponse { balances }
     }
+}
+
+fn get_account_balances(
+    set: impl LedgerSet,
+    args: &AccountsBalancesArgs,
+) -> AccountsBalancesResponse {
+    let mut balances = HashMap::new();
+
+    for account in &args.accounts {
+        let balance = set.account_balance(account);
+        let pending = set.account_receivable(account);
+
+        balances.insert(
+            *account,
+            AccountBalanceResponse {
+                balance,
+                pending,
+                receivable: pending,
+            },
+        );
+    }
+
+    AccountsBalancesResponse { balances }
 }
