afd546ca62861d57dd773ead0323784fd6f78025
Gustav Schauwecker <service@simpago.com>
2025-02-28 11:22:30 +0100

    Add tests for telemetry RPC

--- rpc_server/src/command_handler/node/telemetry.rs
+++ rpc_server/src/command_handler/node/telemetry.rs
@@ -1,5 +1,5 @@
 use crate::command_handler::RpcCommandHandler;
-use anyhow::bail;
+use anyhow::{anyhow, bail};
 use rsnano_rpc_messages::{TelemetryArgs, TelemetryDto, TelemetryResponse};
 use std::net::SocketAddrV6;
 
@@ -7,26 +7,18 @@ impl RpcCommandHandler {
     pub(crate) fn telemetry(&self, args: TelemetryArgs) -> anyhow::Result<TelemetryResponse> {
         let mut responses = Vec::new();
         if args.address.is_some() || args.port.is_some() {
-            // Check both are specified
-            let Some(address) = args.address else {
-                bail!("Both port and address required");
-            };
-            let Some(port) = args.port else {
-                bail!("Both port and address required");
-            };
+            let endpoint = Self::get_endpoint(&args)?;
 
-            let endpoint = SocketAddrV6::new(address, port.into(), 0, 0);
-
-            if address.is_loopback() && port == self.node.tcp_listener.local_address().port().into()
-            {
+            if self.is_local_address(&endpoint) {
                 // Requesting telemetry metrics locally
                 let data = self.node.telemetry.local_telemetry();
                 responses.push(TelemetryDto::from(data));
-                return Ok(TelemetryResponse { metrics: responses });
             } else {
-                let Some(telemetry) = self.node.telemetry.get_telemetry(&endpoint) else {
-                    bail!("Peer not found");
-                };
+                let telemetry = self
+                    .node
+                    .telemetry
+                    .get_telemetry(&endpoint)
+                    .ok_or_else(|| anyhow!("Peer not found"))?;
 
                 responses.push(TelemetryDto::from(telemetry));
             }
@@ -34,8 +26,8 @@ impl RpcCommandHandler {
             // By default, local telemetry metrics are returned,
             // setting "raw" to true returns metrics from all nodes requested.
             let output_raw = args.raw.unwrap_or_default().inner();
-            let all_telemetries = self.node.telemetry.get_all_telemetries();
             if output_raw {
+                let all_telemetries = self.node.telemetry.get_all_telemetries();
                 for (addr, data) in all_telemetries {
                     let mut metric = TelemetryDto::from(data);
                     metric.address = Some(addr.ip().clone());
@@ -50,5 +42,51 @@ impl RpcCommandHandler {
         }
 
         Ok(TelemetryResponse { metrics: responses })
+    }
+
+    fn get_endpoint(args: &TelemetryArgs) -> anyhow::Result<SocketAddrV6> {
+        let Some(address) = args.address else {
+            bail!("Both port and address required");
+        };
+        let Some(port) = args.port else {
+            bail!("Both port and address required");
+        };
+
+        Ok(SocketAddrV6::new(address, port.into(), 0, 0))
+    }
+
+    fn is_local_address(&self, addr: &SocketAddrV6) -> bool {
+        addr.ip().is_loopback() && addr.port() == self.node.tcp_listener.local_address().port()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::Ipv6Addr;
+
+    use rsnano_rpc_messages::{RpcCommand, RpcError, TelemetryArgs};
+
+    use crate::command_handler::test_rpc_command;
+
+    #[test]
+    fn fails_when_only_port_provided() {
+        let cmd = RpcCommand::Telemetry(TelemetryArgs {
+            raw: None,
+            address: None,
+            port: Some(123.into()),
+        });
+        let error: RpcError = test_rpc_command(cmd);
+        assert_eq!(error.error, "Both port and address required")
+    }
+
+    #[test]
+    fn fails_when_only_address_provided() {
+        let cmd = RpcCommand::Telemetry(TelemetryArgs {
+            raw: None,
+            address: Some(Ipv6Addr::LOCALHOST),
+            port: None,
+        });
+        let error: RpcError = test_rpc_command(cmd);
+        assert_eq!(error.error, "Both port and address required")
     }
 }
