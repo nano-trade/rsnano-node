0cf4d5e002839c458c96bbfec865b0578fe54502
Gustav Schauwecker <service@simpago.com>
2025-03-10 16:12:39 +0100

    Refactor active elections

--- node/src/consensus/active_elections/mod.rs
+++ node/src/consensus/active_elections/mod.rs
@@ -262,72 +262,6 @@ impl ActiveElections {
         self.active_root(&block.qualified_root())
     }
 
-    fn replace_by_weight<'a>(
-        &self,
-        election_mutex: &'a Mutex<Election>,
-        mut election: MutexGuard<'a, Election>,
-        hash: &BlockHash,
-    ) -> (bool, MutexGuard<'a, Election>) {
-        let mut replaced_block = BlockHash::zero();
-        let winner_hash = election.winner_hash().unwrap();
-        // Sort existing blocks tally
-        let mut sorted: Vec<_> = election
-            .last_tally
-            .iter()
-            .map(|(hash, amount)| (*hash, *amount))
-            .collect();
-        drop(election);
-
-        // Sort in ascending order
-        sorted.sort_by(|left, right| right.cmp(left));
-
-        let votes_tally = |votes: &[Arc<Vote>]| {
-            let mut result = Amount::zero();
-            for vote in votes {
-                result += self.rep_weights.weight(&vote.voting_account);
-            }
-            result
-        };
-
-        // Replace if lowest tally is below inactive cache new block weight
-        let inactive_existing = self.vote_cache.lock().unwrap().find(hash);
-        let inactive_tally = votes_tally(&inactive_existing);
-        if inactive_tally > Amount::zero() && sorted.len() < ELECTION_MAX_BLOCKS {
-            // If count of tally items is less than 10, remove any block without tally
-            let guard = election_mutex.lock().unwrap();
-            for (hash, _) in &guard.last_blocks {
-                if sorted.iter().all(|(h, _)| h != hash) && *hash != winner_hash {
-                    replaced_block = *hash;
-                    break;
-                }
-            }
-        } else if inactive_tally > Amount::zero() && inactive_tally > sorted.first().unwrap().1 {
-            if sorted.first().unwrap().0 != winner_hash {
-                replaced_block = sorted[0].0;
-            } else if inactive_tally > sorted[1].1 {
-                // Avoid removing winner
-                replaced_block = sorted[1].0;
-            }
-        }
-
-        let mut replaced = false;
-        if !replaced_block.is_zero() {
-            self.vote_router.disconnect(&replaced_block);
-            election = election_mutex.lock().unwrap();
-            self.remove_block(&mut election, &replaced_block);
-            replaced = true;
-        } else {
-            election = election_mutex.lock().unwrap();
-        }
-        (replaced, election)
-    }
-
-    fn remove_block(&self, election: &mut Election, hash: &BlockHash) {
-        if let Some(block) = election.remove_block(hash) {
-            self.clear_publish_filter(&block);
-        }
-    }
-
     /// Result is true if:
     /// 1) election is confirmed or expired
     /// 2) given election contains 10 blocks & new block didn't receive enough votes to replace existing blocks
@@ -343,9 +277,11 @@ impl ActiveElections {
         if election.last_blocks.len() >= ELECTION_MAX_BLOCKS
             && !election.last_blocks.contains_key(&fork.hash())
         {
-            let (replaced, guard) = self.replace_by_weight(election_mutex, election, &fork.hash());
-            election = guard;
-            if !replaced {
+            let replaced = self.replace_by_weight(&mut election, &fork.hash());
+            if let Some(replaced) = replaced {
+                self.vote_router.disconnect(&replaced.hash());
+                self.clear_publish_filter(&replaced);
+            } else {
                 self.clear_publish_filter(fork);
                 return true;
             }
@@ -371,6 +307,64 @@ impl ActiveElections {
             .insert(fork.hash(), MaybeSavedBlock::Unsaved(fork.clone()));
 
         false
+    }
+
+    fn replace_by_weight<'a>(
+        &self,
+        election: &mut Election,
+        fork_hash: &BlockHash,
+    ) -> Option<MaybeSavedBlock> {
+        let mut replaced_block_hash = BlockHash::zero();
+        let winner_hash = election.winner_hash().unwrap();
+        // Sort existing blocks tally
+        let mut sorted: Vec<_> = election
+            .last_tally
+            .iter()
+            .map(|(hash, amount)| (*hash, *amount))
+            .collect();
+
+        // Sort in ascending order
+        sorted.sort_by(|left, right| right.cmp(left));
+
+        let weights = self.rep_weights.read();
+        let votes_tally = |votes: &[Arc<Vote>]| {
+            let mut result = Amount::zero();
+            for vote in votes {
+                result += weights
+                    .get(&vote.voting_account)
+                    .cloned()
+                    .unwrap_or_default();
+            }
+            result
+        };
+
+        // Replace if lowest tally is below inactive cache new block weight
+        let inactive_existing = self.vote_cache.lock().unwrap().find(fork_hash);
+        let inactive_tally = votes_tally(&inactive_existing);
+        if inactive_tally > Amount::zero() && sorted.len() < ELECTION_MAX_BLOCKS {
+            // If count of tally items is less than 10, remove any block without tally
+            for (hash, _) in &election.last_blocks {
+                if sorted.iter().all(|(h, _)| h != hash) && *hash != winner_hash {
+                    replaced_block_hash = *hash;
+                    break;
+                }
+            }
+        } else if inactive_tally > Amount::zero() && inactive_tally > sorted.first().unwrap().1 {
+            if sorted.first().unwrap().0 != winner_hash {
+                replaced_block_hash = sorted[0].0;
+            } else if inactive_tally > sorted[1].1 {
+                // Avoid removing winner
+                replaced_block_hash = sorted[1].0;
+            }
+        }
+
+        let replaced = if !replaced_block_hash.is_zero() {
+            election.remove_block(&replaced_block_hash)
+        } else {
+            None
+        };
+
+        replaced
     }
 
     /// Broadcasts vote for the current winner of this election
