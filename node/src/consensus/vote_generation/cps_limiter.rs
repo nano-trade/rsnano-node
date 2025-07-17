45aa9307c9251236848736596ba87b6c3e8d4a73
Gustav Schauwecker <service@simpago.com>
2025-07-17 17:41:31 +0200

    Work on cps limiter

--- node/src/consensus/vote_generation/cps_limiter.rs
+++ node/src/consensus/vote_generation/cps_limiter.rs
@@ -1,33 +1,88 @@
+use crate::block_rate_calculator::CurrentBlockRates;
+use rsnano_network::token_bucket::TokenBucket;
+use rsnano_nullable_clock::Timestamp;
+use std::{sync::Arc, time::Duration};
+
 /// Limits the amount of created final votes with the goal of
 /// reducing the network confirmation rate to a configured limit
-#[derive(Default)]
 pub(crate) struct CpsLimiter {
-    call_count: usize,
+    block_rates: Arc<CurrentBlockRates>,
+    cps_limit: f64,
+    current_rate: f64,
+    limiter: TokenBucket,
+    last_adjustment: Option<Timestamp>,
 }
 
 impl CpsLimiter {
-    pub fn try_vote(&mut self) -> bool {
-        self.call_count += 1;
-        // TODO
-        true
+    const RATE_ADJUSTMENT_INTERVAL: Duration = Duration::from_secs(15);
+
+    pub(crate) fn new(block_rates: Arc<CurrentBlockRates>, cps_limit: usize) -> Self {
+        Self {
+            block_rates,
+            cps_limit: cps_limit as f64,
+            current_rate: cps_limit as f64,
+            limiter: TokenBucket::new(cps_limit),
+            last_adjustment: None,
+        }
     }
-    pub fn call_count(&self) -> usize {
-        self.call_count
+
+    pub fn try_vote(&mut self, now: Timestamp) -> bool {
+        // TODO unlimited
+        if self.should_adjust_rate(now) {
+            let cps = self.block_rates.cps() as f64;
+            self.current_rate = calculate_new_limiter_rate(self.current_rate, cps, self.cps_limit);
+            self.limiter.set_limit(self.current_rate as usize);
+        }
+
+        self.limiter.try_consume(1, now)
     }
+
+    fn should_adjust_rate(&self, now: Timestamp) -> bool {
+        match self.last_adjustment {
+            Some(last) => last.elapsed(now) >= Self::RATE_ADJUSTMENT_INTERVAL,
+            None => true,
+        }
+    }
+}
+
+fn calculate_new_limiter_rate(current_rate: f64, cps: f64, cps_limit: f64) -> f64 {
+    // Limit vote creation to 100x of CPS limit. This is done to prevent the token bucket limit
+    // to grow unbounded.
+    let max = cps_limit * 100.0;
+
+    // The lower limit is introduced, so that CPS rate recovers quicker after a sharp drop
+    let min = (cps_limit * 0.3).max(1.0);
+
+    // Adjustment factor for token buckent increase/decrease
+    const ALPHA: f64 = 0.2;
+
+    (current_rate * (1.0 + ALPHA * (1.0 - cps / cps_limit))).clamp(min, max)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ntest::assert_true;
+    use ntest::{assert_false, assert_true};
 
     #[test]
-    fn track_calls() {
-        let mut limiter = CpsLimiter::default();
-        assert_eq!(limiter.call_count(), 0);
-        assert_true!(limiter.try_vote());
-        assert_eq!(limiter.call_count(), 1);
-        assert_true!(limiter.try_vote());
-        assert_eq!(limiter.call_count(), 2);
+    fn vote_when_below_cps_threshold() {
+        let block_rates = Arc::new(CurrentBlockRates::default());
+        const CPS_LIMIT: usize = 1000;
+        let now = Timestamp::new_test_instance();
+        let mut limiter = CpsLimiter::new(block_rates, CPS_LIMIT);
+        assert_true!(limiter.try_vote(now));
+        assert_true!(limiter.try_vote(now));
+    }
+
+    #[test]
+    fn dont_vote_when_above_threshold() {
+        let block_rates = Arc::new(CurrentBlockRates::default());
+        const CPS_LIMIT: usize = 1;
+        let now = Timestamp::new_test_instance();
+        let mut limiter = CpsLimiter::new(block_rates.clone(), CPS_LIMIT);
+        assert_true!(limiter.try_vote(now));
+
+        block_rates.set_cps(1000);
+        assert_false!(limiter.try_vote(now));
     }
 }
